51f252c07a4155f2a1c6401d0297097107c5cc56
RuBin <rubinnaw@gmail.com>
2024-03-04 11:28:28 +0700

    ....

--- frontend/src/scripts/constants.ts
+++ frontend/src/scripts/constants.ts
@@ -1,6 +1,6 @@
 import {UserRole} from "@/components/pages/workroom/workRoom";
 
-export const homeUrl: string = 'http://172.16.1.63:8081'
+export const homeUrl: string = 'http://192.168.84.96:8081'
 export const domain: string = `${homeUrl}/api/v1/`
 export const APIS = {
     API : `${domain}api/v1/`,
